87eee9c4bb0ebef445f6fa9153d0a92d520f97ae
Wonicon <conciselove@outlook.com>
2016-01-23 22:20:41 +0800

    Implement crowbar basic front end

---
+++ crowbar/CRB.h
@@ -0,0 +1,10 @@
+#ifndef PUBLIC_CRB_H_INCLUDED
+#define PUBLIC_CRB_H_INCLUDED
+
+#include <stdio.h>
+
+typedef struct CRB_Interpreter_tag CRB_Interpreter;
+
+CRB_Interpreter *CRB_create_interpreter();
+
+#endif // PUBLIC_CRB_H_INCLUDED
---
+++ crowbar/crowbar.h
@@ -0,0 +1,10 @@
+#ifndef PRIVATE_CROWBAR_H_INCLUDED
+#define PRIVATE_CROWBAR_H_INCLUDED
+
+#include <stdio.h>
+#include "CRB.h"
+
+#define min(a,b) ((a) < (b) ? (a) : (b))
+#define max(a,b) ((a) > (b) ? (a) : (b))
+
+#endif // PRIVATE_CROWBAR_H_INCLUDED
