c75565df2f94635aad3962086e71c3f543721d18
Wonicon <conciselove@outlook.com>
2016-01-25 00:30:24 +0800

    Add testcase

--- crowbar/main.c
+++ crowbar/main.c
@@ -5,5 +5,17 @@ int main(int argc, char *argv[])
 {
     extern int yyparse();
     extern FILE *yyin;
+
+#ifdef DEBUG
+    const char *file = "test/test.crb";
+#else
+    const char *file = argv[1];
+#endif // DEBUG
+
+    yyin = fopen(file, "r");
+    if (yyin == NULL) {
+        fprintf(stderr, "source file %s not found!\n", file);
+    }
     yyparse();
+    return 0;
 }
