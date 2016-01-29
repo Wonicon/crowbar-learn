b781074d09875985057b1912693333e6d9503a91
Wonicon <conciselove@outlook.com>
2016-01-29 10:10:21 +0800

    Update MEM_realloc_func

--- crowbar/memory/memory.c
+++ crowbar/memory/memory.c
@@ -23,6 +23,20 @@ union Header_tag {
 #define MARK 0xCD
 
 
+static void
+error_handler(MEM_Controller controller, char *filename, int line, const char *msg)
+{
+    if (controller->error_fp == NULL) {
+        controller->error_fp = stderr;
+    }
+    controller->error_handler(controller, filename, line, msg);
+
+    if (controller->fail_mode == MEM_FAIL_AND_EXIT) {
+        exit(1);
+    }
+}
+
+
 static struct MEM_Controller_tag st_default_controller;
 MEM_Controller mem_default_controller = &st_default_controller;
 
@@ -159,5 +173,67 @@ MEM_malloc_func(MEM_Controller controller, const char *filename, int line, size_
 #endif
 
     return ptr;
+}
+
+
+void *
+MEM_realloc_func(MEM_Controller controller, const char *filename, int line, void *ptr, size_t size)
+{
+    void *new_ptr;
+    size_t alloc_size;
+    void *real_ptr;
+#ifdef DEBUG
+    Header old_header;
+    int old_size;
+
+    alloc_size = size + sizeof(Header) + MARK_SIZE;
+    if (ptr != NULL) {
+        real_ptr = (char *)ptr - sizeof(Header);
+        check_mark((Header *)real_ptr);
+        old_header = *(Header *)real_ptr;  // Save the header onto stack.
+        old_size = old_header.size;
+        unchain_block(controller, (Header *)real_ptr);
+    }
+    else {
+        real_ptr = NULL;
+        old_size = 0;
+    }
+#else
+    alloc_size = size;
+    real_ptr = ptr;
+#endif
+
+    new_ptr = realloc(real_ptr, alloc_size);
+    if (new_ptr == NULL) {
+        if (real_ptr == NULL) {
+            error_handler(controller, filename, line, "realloc(malloc)");
+        }
+        else {
+            error_handler(controller, filename, line, "realloc");
+        }
+    }
+
+#ifdef DEBUG
+    if (real_ptr != NULL) {
+        // realloc
+        *(Header *)new_ptr = old_header;
+        ((Header *)new_ptr)->size = size;
+        // Relink to where it used to be linked.
+        rechain_block(controller, (Header *)new_ptr);
+        set_tail(new_ptr, alloc_size);
+    }
+    else {
+        // realloc acts as malloc
+        set_header(new_ptr, size, filename, line);
+        set_tail(new_ptr, alloc_size);
+        chain_block(controller, (Header *)new_ptr);
+    }
+    new_ptr = (char *)new_ptr + sizeof(Header);
+    if (size > old_size) {
+        memset((char *)new_ptr + old_size, 0xCC, size - old_size);
+    }
+#endif
+
+    return new_ptr;
 }
 
